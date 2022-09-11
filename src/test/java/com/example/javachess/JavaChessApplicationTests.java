4bb14ae5aa802eae17944de936c5ddf7d926ee07
mins1031 <urisegea@naver.com>
2022-09-11 18:06:58 +0900

    first commit

---
+++ src/test/java/com/example/javachess/JavaChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javachess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
