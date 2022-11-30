5b9c0b4d6e267735522e98f5a82b2540b8228d1b
mins1031 <urisegea@naver.com>
2022-11-30 23:47:19 +0900

    feat: 구성 변경

--- src/main/java/com/example/javachess/console/board/ChessBoard.java
+++ src/main/java/com/example/javachess/console/board/ChessBoard.java
@@ -2,16 +2,19 @@ package com.example.javachess.console.board;
 
 import com.example.javachess.console.piece.Piece;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 @Getter
+@NoArgsConstructor
 public class ChessBoard {
     //일급컬렉션에 대해 알아보고 map 보드를 전체 get 할지, 특정위치의 상황 get and 모든 위치의 상태 toString 처럼 get 하는 두개의 get 메서드 만들지?
 
     //2차원 배열보다는 map이 낫다. 전자는 특정위치를 찾기위해 2중포문을 돌겠지만 후자는 key값을 찾으면 되기에 시간 복잡도가 n^2 vs n 이다.
-    private Map<String, Piece> chessBoard = new TreeMap<>();
+    private Map<String, Piece> chessBoard = new HashMap<>();
 
     public void initChessBoard() {
         StringBuilder tempPointName = new StringBuilder();
